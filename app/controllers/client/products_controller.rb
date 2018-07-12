af18b637d4cf5f43eda1e16519eca341eec5588b
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-07-12 18:21:17 -0400

    implement frontend index route, action and view in client namespace

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -1,2 +1,7 @@
 class Client::ProductsController < ApplicationController
+  def index
+    response = Unirest.get("localhost:3000/api/products")
+    @products = response.body
+    render "index.html.erb"
+  end
 end
